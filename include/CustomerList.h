770b498dd9328b44bb1d44613c86c30cb5f319ef
Yohannes  Kassa <83584566+yohannes-jo@users.noreply.github.com>
2021-08-28 17:03:19 +0000

    finished customer listing

--- include/CustomerList.h
+++ include/CustomerList.h
@@ -4,19 +4,91 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+// 
+#include "MainMenu.h"
 
 using namespace std;
 
-void customer_list(Customer& customers)
+enum ORDER {NORM, NAME_ASC, NAME_DES, AGE_ASC, AGE_DES};
+
+void customer_list(Customer& customers[], ORDER order = NORM)
 {
     system("clear");
     
     cout << "########################" << endl;
-    cout << setw(5) << "ID" << '|' << setw(16) << "Name" << setw(8) << "Balance" << endl;
+    cout << setw(5) << "ID" << '|' << setw(16) << "Name" << '|' << setw(3) << "Age" << '|' << setw(8) << "Balance" << endl;
     cout << "########################" << endl;
 
-    for (Customer& customer : customers)
-        cout << setw(5) << customer.id << '|' << setw(16) << customer.name << '|' << setw(8) << customer.balance << endl;
+    switch (order)
+    {
+    case NORM:
+        for (auto& customer : customers)
+            cout << setw(5) << customer.id << '|' << setw(16) << customer.name << '|' << setw(3) << customer.age << '|' << setw(8) << customer.balance << endl;
+        break;
+    
+    case NAME_ASC:
+        Customer n_a_customers[] = customers;
+        for (int i = 0; i < n_a_customers.size(); i++)
+            for (int j = 0; j < i; j++)
+            {
+                if (n_a_customers[i].name[0] < n_a_customers[j].name[0])
+                {
+                    string temp = n_a_customers[i].name;
+                    n_a_customers[i].name = n_a_customers[j].name;
+                    n_a_customers[j].name = temp;
+                }
+            }
+        
+        customer_list(n_a_customers);
+        break;
+
+    case NAME_DES:
+        Customer n_d_customers[] = customers;
+        for (int i = 0; i < n_d_customers.size(); i++)
+            for (int j = 0; j < i; j++)
+            {
+                if (n_d_customers[i].name[0] > n_d_customers[j].name[0])
+                {
+                    string temp = n_d_customers[i].name;
+                    n_d_customers[i].name = n_d_customers[j].name;
+                    n_d_customers[j].name = temp;
+                }
+            }
+        
+        customer_list(n_d_customers);
+        break;
+    
+    case AGE_ASC:
+        Customer a_a_customers[] = customers;
+        for (int i = 0; i < a_a_customers.size(); i++)
+            for (int j = 0; j < i; j++)
+            {
+                if (a_a_customers[i].age < a_a_customers[j].age)
+                {
+                    string temp = a_a_customers[i].age;
+                    a_a_customers[i].age = a_a_customers[j].age;
+                    a_a_customers[j].age = temp;
+                }
+            }
+        
+        customer_list(a_a_customers);
+        break;
+    
+    case AGE_DES:
+        Customer a_d_customers[] = customers;
+        for (int i = 0; i < a_d_customers.size(); i++)
+            for (int j = 0; j < i; j++)
+            {
+                if (a_d_customers[i].age > a_d_customers[j].age)
+                {
+                    string temp = a_d_customers[i].age;
+                    a_d_customers[i].age = a_d_customers[j].age;
+                    a_d_customers[j].age = temp;
+                }
+            }
+        
+        customer_list(a_d_customers);
+    }
 
     cout << "########################" << endl << endl;
 
@@ -25,12 +97,13 @@ void customer_list(Customer& customers)
     cout << "3. Sort by..." << endl;
     cout << "4. Return to main menu" << endl;
 
+    INPUT0:
     cout << "\n > "; unsigned choice; cin >> choice;
 
     switch (choice)
     {
     case 1:
-        INPUT:
+        INPUT1:
         cout << "\nEnter customer ID: "; int ID; cin >> ID;
         for (auto& customer : customers)
         {
@@ -62,12 +135,63 @@ void customer_list(Customer& customers)
 
         // If customer wasn't found...
         cout << "\nInvalid customer ID. Try again." << endl;
-        goto INPUT;
-
+        goto INPUT1;
         break;
     
     case 2:
+        INPUT2:
+        cout << "\nEnter Customer ID: "; int ID; cin >> ID;
+        for (auto& customer : customers)
+        {
+            if (customer.id == ID)
+            {
+                delete_customer(customers, customer)
+            }
+        }
+
+        // If customer wasn't found...
+        cout << "\nInvalid customer ID. Try again." << endl;
+        goto INPUT2;
+        break;
+    
+    case 3:
+        cout << endl;
+        cout << "1. Name (1.1 for ascending, 1.2 descending)" << endl;
+        cout << "2. Age (2.1 for ascending, 2.2 descending)" << endl;
+
+        INPUT3:
+        cout << "\n > "; float prompt; cin >> prompt;
+        switch (prompt)
+        {
+        case 1.1:
+            customer_list(customers, NAME_ASC);
+            break;
+
+        case 1.2:
+            customer_list(customers, NAME_DES);
+            break;
+
+        case 2.1:
+            customer_list(customers, AGE_ASC);
+            break;
+
+        case 2.2:
+            customer_list(customers, AGE_DES);
+            break;
         
+        default:
+            cout << "\nInvalid input. Try again." << endl;
+            goto INPUT3;
+        }
+        break;
+    
+    case 4:
+        mainMenu();
+        break;
+    
+    default:
+        cout << "\nInvalid input. Try again." << endl;
+        goto INPUT0;
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -1,53 +1,6 @@
-#include <iostream>
-
-#include "include/CustomerList.h"
-
-using namespace std;
+#include "include/MainMenu.h"
 
 int main()
 {
-    system("clear");
-
-    cout << "##############################################" << endl << endl;
-    cout << "######### CINEMA REGISTRATION SYSTEM #########" << endl << endl;
-    cout << "##############################################" << endl;
-    cout << "# 1. MOVIE REGISTRATION                      #" << endl;
-    cout << "# 2. CUSTOMER REGISTRATION                   #" << endl;
-    cout << "# 3. MOVIES LIST                             #" << endl;
-    cout << "# 4. CUSTOMER LIST                           #" << endl;
-    cout << "# 5. SEAT REGISTRATION                       #" << endl;
-    cout << "# 6. DAILY PROFIT REPORT                     #" << endl;
-    cout << "# 7. QUIT                                    #" << endl;
-    cout << "##############################################" << endl;
-
-INPUT0:
-    cout << "\n > "; unsigned choice; cin >> choice;
-
-    switch (choice)
-    {
-    case 1:
-        // movie_registration();
-        break;
-    case 2:
-        // customer_registration();
-        break;
-    case 3:
-        // movie_list();
-        break;
-    case 4:
-        customer_list();
-        break;
-    case 5:
-        // seat_reservation();
-        break;
-    case 6:
-        // profit_report();
-        break;
-    case 7:
-        cout << "Closing..." << endl;
-        return 0;
-    default:
-        cout << "\n*Invalid Input*" << endl;
-        goto INPUT0;
-    }
+    mainMenu();
 }
